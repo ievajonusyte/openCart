bc876657b3148408f0b494dc33214ffb412c6e1d
Ieva Jonusyte <75742395+ievajonusyte@users.noreply.github.com>
2021-07-15 20:58:37 +0300

    Update Utils.java

--- src/main/java/com/openCart/qa/Utils/Utils.java
+++ src/main/java/com/openCart/qa/Utils/Utils.java
@@ -1,5 +1,5 @@
 package com.openCart.qa.Utils;
 
-public class Utils {
+public class Utils{
     public static String BASE_URL = "https://demo.opencart.com";
     public static String CHROME_DRIVER_LOCATION = "chromedriver";}
